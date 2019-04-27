9e96068d33f4641f72924d38ce291bc52310e416
Zhou Xuanzhuo <358409584@qq.com>
2019-04-27 11:36:42 +0800

    详情页第二块面板完成

--- lib/model/ItemList.dart
+++ lib/model/ItemList.dart
@@ -7,40 +7,111 @@ class ItemList extends StatefulWidget {
 }
 
 class _ItemListState extends State<ItemList> {
+  double _sliderValue = 0.0;
+
+  List<int> hpValue(x, lv) {
+    int min;
+    int max;
+    min = (x * 2 * lv / 100 + lv + 10).toInt();
+    max = ((x * 2 + 31 + 63) * lv / 100 + lv + 10).toInt();
+    return [min, max];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        RacialValue(
+            name: 'HP',
+            value: 150,
+            color: Color(0xff8ac654),
+            valueScope: hpValue(150, _sliderValue)),
+        RacialValue(
+            name: '物攻',
+            value: 150,
+            color: Color(0xfff8cb3c),
+            valueScope: hpValue(150, _sliderValue)),
+        RacialValue(
+            name: '物防',
+            value: 150,
+            color: Color(0xffd98837),
+            valueScope: hpValue(150, _sliderValue)),
+        RacialValue(
+            name: '特攻',
+            value: 150,
+            color: Color(0xff59c3d0),
+            valueScope: hpValue(150, _sliderValue)),
+        RacialValue(
+            name: '特防',
+            value: 150,
+            color: Color(0xff5890cd),
+            valueScope: hpValue(150, _sliderValue)),
+        RacialValue(
+            name: '速度',
+            value: 150,
+            color: Color(0xffa456d0),
+            valueScope: hpValue(150, _sliderValue)),
+        Slider(
+          value: _sliderValue,
+          min: 1,
+          max: 100,
+          onChanged: (x) {
+            setState(() {
+              _sliderValue = x;
+            });
+          },
+          label: '${_sliderValue.toInt()}',
+          divisions: 99,
+        ),
+        Text('${_sliderValue.toInt()}')
+      ],
+    );
+  }
+}
+
+class RacialValue extends StatelessWidget {
+  RacialValue(
+      {Key key,
+      @required this.name,
+      @required this.value,
+      @required this.color,
+      @required this.valueScope})
+      : super(key: key);
+  final String name;
+  final int value;
+  final Color color;
+  final List<int> valueScope;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(2),
+      padding: EdgeInsets.all(1),
       child: Row(
         children: <Widget>[
-          Expanded(flex: 1, child: Text('HP')),
-          Expanded(flex: 1, child: Text('255')),
+          Expanded(flex: 1, child: Text(name)),
+          Container(width: 10),
+          Expanded(flex: 1, child: Text('$value')),
+          Container(width: 10),
           Expanded(
             flex: 8,
             child: SizedBox(
               height: 16,
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                              child: Row(
+                borderRadius: BorderRadius.circular(6),
+                child: Row(
                   children: <Widget>[
-                    Expanded(flex: 1, child: box(Colors.blueGrey)),
-                    Expanded(flex: 1, child: box(Colors.red)),
+                    Expanded(flex: value, child: Container(color: color)),
+                    Expanded(
+                        flex: 255 - value,
+                        child: Container(color: color.withOpacity(0.3))),
                   ],
                 ),
               ),
             ),
           ),
-          Expanded(flex: 4, child: Text('111')),
+          Container(width: 10),
+          Expanded(flex: 3, child: Text('${valueScope[0]}~${valueScope[1]}')),
         ],
       ),
     );
   }
-}
-
-Widget box(Color color) {
-  return Container(
-    width: double.infinity,
-    height: double.infinity,
-    decoration: BoxDecoration(color: color),
-  );
 }
--- lib/model/PokeDetail.dart
+++ lib/model/PokeDetail.dart
@@ -25,25 +25,6 @@ class PokeItem extends StatelessWidget {
   PokeItem({Key key, this.index}) : super(key: key);
   final int index;
 
-  Widget ablityButton(index, n) {
-    return Expanded(
-      child: Padding(
-        child: pokemonList[index]['特性'][n] == null
-            ? RaisedButton(
-                onPressed: () {},
-              )
-            : RaisedButton(
-                onPressed: () {
-                  print(abilitiesList[pokemonList[index]['特性'][n] - 1]['简介']);
-                },
-                child: Text(
-                    '${abilitiesList[pokemonList[index]['特性'][n] - 1]['中文名称']}'),
-              ),
-        padding: EdgeInsets.only(right: 2, left: 2),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,52 +42,142 @@ class PokeItem extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         children: <Widget>[
           DetailCardOne(index: index),
-          MyCard(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text('普通特性'),
-                    flex: 2,
-                  ),
-                  Expanded(
-                    child: Text('梦特性'),
-                    flex: 1,
-                  )
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  ablityButton(index, 0),
-                  ablityButton(index, 1),
-                  ablityButton(index, 2),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text('种族值'),
-                    flex: 4,
-                  ),
-                  Expanded(
-                    child: Text('50级'),
-                    flex: 1,
-                  ),
-                  Expanded(
-                    child: Text('100级'),
-                    flex: 1,
-                  ),
-                ],
-              ),
-              Column(
-                children: <Widget>[
+          DetailCardTwo(index: index),
+        ],
+      ),
+    );
+  }
+}
 
-                ],
+//第二部分卡片
+class DetailCardTwo extends StatefulWidget {
+  DetailCardTwo({Key key, this.index}) : super(key: key);
+  final int index;
+  @override
+  _DetailCardTwoState createState() => _DetailCardTwoState();
+}
+
+class _DetailCardTwoState extends State<DetailCardTwo> {
+  Widget ablityButton(index, n) {
+    return Expanded(
+      child: Padding(
+        child: pokemonList[index]['特性'][n] == null
+            ? RaisedButton(
+                color: Colors.blue[200],
+                onPressed: () {},
+              )
+            : RaisedButton(
+                color: Colors.blue[200],
+                onPressed: () {
+                  print(abilitiesList[pokemonList[index]['特性'][n] - 1]['简介']);
+                },
+                child: Text(
+                    '${abilitiesList[pokemonList[index]['特性'][n] - 1]['中文名称']}'),
+              ),
+        padding: EdgeInsets.only(right: 2, left: 2),
+      ),
+    );
+  }
+
+  Widget effortsValue(value) {
+    return 
+      Expanded(
+        child: Card(
+          color: Colors.blue[200],
+          child: Text('$value', textAlign: TextAlign.center),
+          
+        ),
+        flex: 1,
+      )
+    ;
+  }
+
+  double _sliderValue = 50;
+
+  @override
+  Widget build(BuildContext context) {
+    return MyCard(
+      color: Colors.blue[100],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            //特性标题
+            children: <Widget>[
+              Expanded(
+                child: Text('普通特性'),
+                flex: 2,
+              ),
+              Expanded(
+                child: Text('梦特性'),
+                flex: 1,
+              )
+            ],
+          ),
+          Row(
+            //特性内容
+            children: <Widget>[
+              ablityButton(widget.index, 0),
+              ablityButton(widget.index, 1),
+              ablityButton(widget.index, 2),
+            ],
+          ),
+          Divider(
+            color: Colors.white,
+            height: 15.0,
+          ),
+          Row(
+            //种族值标题
+            children: <Widget>[
+              Text('种族值'),
+              Expanded(
+                flex: 8,
+                child: Slider(
+                  value: _sliderValue,
+                  min: 1,
+                  max: 100,
+                  onChanged: (x) {
+                    setState(() {
+                      _sliderValue = x;
+                    });
+                  },
+                  label: '${_sliderValue.toInt()}',
+                  divisions: 99,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Text(
+                  '${_sliderValue.toInt()}级',
+                  textAlign: TextAlign.center,
+                ),
               ),
             ],
-          ))
+          ),
+          //种族值内容
+          RacialValueBox(lv: _sliderValue.toInt(), index: widget.index),
+          Divider(
+            color: Colors.white,
+            height: 15.0,
+          ),
+          Text('努力值'),
+          Row(
+            //特性标题
+            children: <Widget>[
+              Text('HP'),
+              effortsValue(pokemonList[widget.index]['努力值'][5]),
+              Text('物攻'),
+              effortsValue(pokemonList[widget.index]['努力值'][4]),
+              Text('物防'),
+              effortsValue(pokemonList[widget.index]['努力值'][3]),
+              Text('特功'),
+              effortsValue(pokemonList[widget.index]['努力值'][2]),
+              Text('特防'),
+              effortsValue(pokemonList[widget.index]['努力值'][1]),
+              Text('速度'),
+              effortsValue(pokemonList[widget.index]['努力值'][0]),
+            ],
+          ),
         ],
       ),
     );
@@ -115,7 +186,7 @@ class PokeItem extends StatelessWidget {
 
 //第一部分卡片，包括图片、名称、种类、属性、高、重
 class DetailCardOne extends StatelessWidget {
-  final index;
+  final int index;
   DetailCardOne({Key key, this.index}) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -183,6 +254,115 @@ class DetailCardOne extends StatelessWidget {
   }
 }
 
+//特定功能部件
+
+//每项种族值
+class RacialValue extends StatelessWidget {
+  RacialValue(
+      {Key key,
+      @required this.name,
+      @required this.value,
+      @required this.color,
+      @required this.valueScope})
+      : super(key: key);
+  final String name;
+  final int value;
+  final Color color;
+  final List<int> valueScope;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(1),
+      child: Row(
+        children: <Widget>[
+          Expanded(flex: 3, child: Text(name)),
+          Expanded(flex: 2, child: Text('$value')),
+          Container(width: 10),
+          Expanded(
+            flex: 15,
+            child: SizedBox(
+              height: 16,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(flex: value, child: Container(color: color)),
+                    Expanded(
+                        flex: 255 - value,
+                        child: Container(color: color.withOpacity(0.3))),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(width: 20),
+          Expanded(flex: 5, child: Text('${valueScope[0]}~${valueScope[1]}')),
+        ],
+      ),
+    );
+  }
+}
+
+//种族值部件
+class RacialValueBox extends StatelessWidget {
+  final int lv;
+  final int index;
+  RacialValueBox({Key key, this.lv, this.index}) : super(key: key);
+  List<int> hpValue(x, lv) {
+    int min;
+    int max;
+    min = (x * 2 * lv / 100 + lv + 10).toInt();
+    max = ((x * 2 + 31 + 63) * lv / 100 + lv + 10).toInt();
+    return [min, max];
+  }
+
+  List<int> otherValue(x, lv) {
+    int min;
+    int max;
+    min = ((x * 2 * lv / 100 + 5) * 0.9).toInt();
+    max = (((x * 2 + 31 + 63) * lv / 100 + 5) * 1.1).toInt();
+    return [min, max];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        RacialValue(
+            name: 'HP',
+            value: pokemonList[index]['种族值'][5],
+            color: Color(0xff8ac654),
+            valueScope: hpValue(pokemonList[index]['种族值'][5], lv)),
+        RacialValue(
+            name: '物攻',
+            value: pokemonList[index]['种族值'][4],
+            color: Color(0xfff8cb3c),
+            valueScope: otherValue(pokemonList[index]['种族值'][4], lv)),
+        RacialValue(
+            name: '物防',
+            value: pokemonList[index]['种族值'][3],
+            color: Color(0xffd98837),
+            valueScope: otherValue(pokemonList[index]['种族值'][3], lv)),
+        RacialValue(
+            name: '特攻',
+            value: pokemonList[index]['种族值'][2],
+            color: Color(0xff59c3d0),
+            valueScope: otherValue(pokemonList[index]['种族值'][2], lv)),
+        RacialValue(
+            name: '特防',
+            value: pokemonList[index]['种族值'][1],
+            color: Color(0xff5890cd),
+            valueScope: otherValue(pokemonList[index]['种族值'][1], lv)),
+        RacialValue(
+            name: '速度',
+            value: pokemonList[index]['种族值'][0],
+            color: Color(0xffa456d0),
+            valueScope: otherValue(pokemonList[index]['种族值'][0], lv)),
+      ],
+    );
+  }
+}
+
 //基础部件
 
 //我的卡片部件，快捷的设置高度、圆角、内边距和颜色
@@ -199,12 +379,12 @@ class MyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: height ?? 200,
+      //height: height ?? 200,
       child: Card(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(radius ?? 8.0)),
           ),
-          color: color ?? Colors.blue[200],
+          color: color ?? Colors.blue[100],
           child: Padding(
             padding: padding ?? EdgeInsets.all(8.0),
             child: child,
