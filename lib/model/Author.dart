767d36dbf476878a1198ad86dae76746b725b104
Zhou Xuanzhuo <358409584@qq.com>
2019-05-09 22:46:49 +0800

    信息

--- lib/model/Author.dart
+++ lib/model/Author.dart
@@ -11,7 +11,18 @@ class AuthorDemo extends StatelessWidget {
         UserAccountsDrawerHeader(
           accountName:
               Text('围巾落地冻成狗', style: TextStyle(fontWeight: FontWeight.bold)),
-          accountEmail: Text('没想好写点什么~'),
+          accountEmail: InkWell(
+              onTap: () {
+                Navigator.pop(context);
+                launch('https://github.com/pma934');
+              },
+              child: Text(
+                'https://github.com/pma934',
+                style: TextStyle(
+                  decoration: TextDecoration.underline,
+                  decorationStyle: TextDecorationStyle.solid,
+                ),
+              )),
           currentAccountPicture: CircleAvatar(
             backgroundImage: AssetImage('lib/assets/bg-2.md.png'),
           ),
@@ -24,24 +35,65 @@ class AuthorDemo extends StatelessWidget {
                     Colors.blue[200].withOpacity(0.1), BlendMode.lighten),
               )),
         ),
-        ListTile(
-            leading: Icon(Icons.message, color: Colors.black12, size: 22.0),
-            title: Text(
-              '项目地址',
-              textAlign: TextAlign.right,
-              style: TextStyle(
-                color: Colors.blueAccent[700],
-                decoration: TextDecoration.underline,
-                decorationStyle: TextDecorationStyle.solid,
-                decorationColor: Colors.blueAccent[700],
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Text('项目地址',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center),
+              Container(height: 5),
+              InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                  launch('https://github.com/pma934/pokedex');
+                },
+                child: Text('https://github.com/pma934/pokedex',
+                    style: TextStyle(
+                      color: Colors.blueAccent[700],
+                      decoration: TextDecoration.underline,
+                      decorationStyle: TextDecorationStyle.solid,
+                      decorationColor: Colors.blueAccent[700],
+                    ),
+                    textAlign: TextAlign.center),
               ),
-            ),
-            trailing: Icon(Icons.message, color: Colors.black12, size: 22.0),
-            onTap: () {
-              Navigator.pop(context);
-              const url = 'https://github.com/pma934/pokedex';
-              launch(url);
-            }),
+              Container(height: 15),
+              Text('数据来源',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center),
+              Container(height: 5),
+              InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                  launch('https://pokeapi.co/');
+                },
+                child: Text('https://pokeapi.co/',
+                    style: TextStyle(
+                      color: Colors.blueAccent[700],
+                      decoration: TextDecoration.underline,
+                      decorationStyle: TextDecorationStyle.solid,
+                      decorationColor: Colors.blueAccent[700],
+                    ),
+                    textAlign: TextAlign.center),
+              ),
+              Container(height: 15),
+              Text('图片来源',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center),
+              Container(height: 5),
+              Text('背景图片来源于p站，精灵图片来源于Pokémon Shuffle和https://pokeapi.co/',
+                  textAlign: TextAlign.center),
+              Container(height: 15),
+              Text('免责声明',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center),
+              Container(height: 5),
+              Text(
+                  '精灵宝可梦相关数据和图片版权归Nintendo所有，本App对精灵宝可梦内容的使用基于著作权法的合理使用原则，绝无侵犯著作权之故意。',
+                  textAlign: TextAlign.center),
+            ],
+          ),
+        )
       ],
     ));
   }
