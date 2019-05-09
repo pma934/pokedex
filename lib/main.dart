3bac60f1413bfb19906b444139c2592ec06eefb7
Zhou Xuanzhuo <358409584@qq.com>
2019-05-09 23:17:11 +0800

    MaterialApp名称

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: '宝可梦图鉴',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
